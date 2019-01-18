3e259bbbf8cd2017e87cb2cf8526f41aa459c815
zhangwu <undergrowth@126.com>
2019-01-18 18:37:47 +0800

    添加 airflow 相关示例

---
+++ test/practice/airflow/example/ssh_run_test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 17),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+}
+
+dag = DAG(
+    'ssh_run_test', default_args=default_args,
+    schedule_interval='*/10 * * * *')
+
+# t1, t2 and t3 are examples of tasks created by instantiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+t2 = SSHOperator(
+    ssh_conn_id='ssh_gzxhop03_lc', # 指定conn_id
+    task_id='ssh_run_script',
+    command='/home/iyourcar_test/tools/test/test_script/hello_world.sh ',
+    # 远程机器上的脚本文件
+    dag=dag)
+
+
+t2.set_upstream(t1)
